1c219e891906fe4dc5f8a2a3364da531f73721b0
Drew <87270938+DrewCodes82@users.noreply.github.com>
2025-08-17 20:02:27 -0400

    currentHistory bug fix

--- force-app/main/default/lwc/platformDevCertCalculator/platformDevCertCalculator.js
+++ force-app/main/default/lwc/platformDevCertCalculator/platformDevCertCalculator.js
@@ -18,6 +18,8 @@ export default class PlatformDevCertCalculator extends LightningElement {
     showResources = false;
     showGoodJob = false;
 
+    currentHistoryId = 0;
+
     attemptHistory = [
         {Id: 1, Score: 50},
         {Id: 2, Score: 68},
@@ -62,11 +64,10 @@ export default class PlatformDevCertCalculator extends LightningElement {
     }
 
     addAttemptHistory(Score){
-        let randomId = Math.floor(Math.random() * 100);
-        console.log(randomId);
+        this.currentHistoryId ++;
         const attempt = 
                 {
-                    Id: this.attemptHistory.length + 1, Score
+                    Id: this.currentHistoryId, Score
                 }
         this.attemptHistory = [...this.attemptHistory, attempt];
     }
@@ -76,6 +77,10 @@ export default class PlatformDevCertCalculator extends LightningElement {
        let attemptId = event.detail;   
        this.attemptHistory = this.attemptHistory.filter(attempt => attempt.Id != attemptId);  
        console.log('New attempt history' + this.attemptHistory)
+    }
+
+    connectedCallback() {
+        this.currentHistoryId = this.attemptHistory.length;
     }
 
 
